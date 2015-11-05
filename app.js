52e2550a4c5c4b5492f41e92773b773f54023383
Unhumanje <petarlaf@gmail.com>
2015-11-05 08:19:13 +0000

    Fixed B2C, Added B2B, Added latest HTML

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
-var pg = require("pg")
+var pg = require("pg");
 var express = require('express');
 var bodyParser = require("body-parser");
 var psql;
@@ -40,22 +40,22 @@ if (process.env.VCAP_SERVICES) {
 	var postgre_username = credentials['username'];
 	var postgre_password = credentials['password']; 
 	var postgre_conn_string = 'postgres://'+postgre_username+':'+postgre_password+'@'+postgre_public_hostname+'/compose';
-};
+}
 
 
 
 app.post('/ask', function (req, res){
 //	calculate cost
 	switch (req.body.country){
-	case "UK" :  tabName = "SaaS_UK_Pricing";
+	case "UK" :  tabName = "saas_uk_pricing";
 			break;
-	case "EUR" : tabName = "SaaS_EUR_Pricing";
+	case "EUR" : tabName = "saas_eur_pricing";
 			break;
-	case "US" :	tabName = "SaaS_US_Pricing";
+	case "US" :	tabName = "saas_us_pricing";
 			break;
-	case "AUS" :	tabName = "SaaS_AUS_Pricing";
+	case "AUS" :	tabName = "saas_aus_pricing";
 			break;
-	};
+	}
 	
 //	branch for products
 	
@@ -66,16 +66,16 @@ app.post('/ask', function (req, res){
 		var q1 = "SELECT quantity_tier,  srp_ref \n" +
 		"FROM public." + tabName + " \n" +
 		"WHERE (part_number = 'D1IKXLL')";
-		console.log("query2: " + q1);
-	
+		console.log("MCB2C query1: " + q1);
+
 		var q2 = "SELECT part_number,  srp_ref \n " +
 		"FROM public." + tabName +  "\n " +
 			"WHERE part_number = 'D1ILBLL' \n " +
 			"OR part_number = 'D1ILILL' \n " +
 			"OR part_number = 'D1ILKLL' \n " +
-			"OR part_number = 'D1ILNLL'";
+			"OR part_number = 'D1ILMLL'";
 
-		console.log("query: " + q2);
+		console.log("MCB2C query2: " + q2);
 		var client = new pg.Client(postgre_conn_string);
 		client.connect(function(err) {
 			if (err) {
@@ -89,15 +89,16 @@ app.post('/ask', function (req, res){
 				}
 				console.log("Output: " + result.rows[0].quantity_tier);
 				var cost = cummulativeTierCalc(result.rows, events);
-				client.query(q2, function(err, result) {
+				
+			client.query(q2, function(err, result) {
 					if (err) {
 						res.end("Error running query: " + err);
-					};
-					console.log("returnedValue: " + returnedValue);
-					var B2CSetup = Number(returnedValue[3].srp_ref);
-					var StandardAccess = returnedValue[0].srp_ref;
-					var mcinsightsP = returnedValue[2].srp_ref;
-					var mcusersP = returnedValue[1].srp_ref;
+					}
+					console.log("returnedValue: " + result);
+					var B2CSetup = Number(result.rows[3]['srp_ref']);
+					var StandardAccess = result.rows[0]['srp_ref'];
+					var mcinsightsP = result.rows[2]['srp_ref'];
+					var mcusersP = result.rows[1]['srp_ref'];
 					
 //					calculates price from returned value
 					var priceEstimateM = (cost +
@@ -115,9 +116,149 @@ app.post('/ask', function (req, res){
 		});
 
 	} else if (req.body.product == "B2B"){
-		
-	};
 
+		//		B2B button was pressed
+
+		var dbRecords = req.body.dbrec/10000;
+
+		var q1 = "SELECT quantity_tier,  srp_ref \n" +
+				"FROM public." + tabName + " \n" +
+				"WHERE (part_number = 'D1ILFLL')";
+		console.log("MCB2B query1: " + q1);
+
+		var q2 = "SELECT part_number,  srp_ref \n " +
+				"FROM public." + tabName +  "\n " +
+				"WHERE part_number = 'D1ILBLL' \n " +
+				"OR part_number = 'D1ILILL' \n " +
+				"OR part_number = 'D1ILKLL' \n " +
+				"OR part_number = 'D1ILNLL'";
+
+		console.log("MCB2B query2: " + q2);
+		var client = new pg.Client(postgre_conn_string);
+		client.connect(function(err) {
+			if (err) {
+				res.end("Could not connect to postgre: " + err);
+// 		    	make all references to res return error
+			}
+			console.log("connection opened");
+			client.query(q1, function(err, result) {
+				if (err) {
+					res.end("Error running query: " + err);
+				}
+				console.log("Output: " + result.rows[0].quantity_tier);
+				var cost = cummulativeTierCalc(result.rows, dbRecords);
+
+				client.query(q2, function(err, result) {
+					if (err) {
+						res.end("Error running query: " + err);
+					}
+					console.log("returnedValue: " + result);
+					var B2BSetup = Number(result.rows[3]['srp_ref']);
+					var StandardAccess = result.rows[0]['srp_ref'];
+					var mcinsightsP = result.rows[2]['srp_ref'];
+					var mcusersP = result.rows[1]['srp_ref'];
+
+                    console.log("B2B setup: " + B2BSetup);
+                    console.log("StandardAccess: " + StandardAccess);
+                    console.log("mcinsightsP: " + mcinsightsP);
+                    console.log("mcusersP: " + mcusersP);
+
+//					calculates price from returned value
+					var priceEstimateM = (cost +
+					Number(StandardAccess) +
+					Number(req.body.mcusers)*Number(mcusersP) +
+					Number(req.body.mcinsights)*Number(mcinsightsP)/100000);
+					var priceEstimateY = priceEstimateM*12;
+                    console.log("Cost: " + cost);
+                    console.log("PriceM: " + priceEstimateM);
+                    console.log("PriceY: " + priceEstimateY);
+                    console.log("b2bsetup: " + B2BSetup);
+
+                    res.send({pricem : priceEstimateM, 	pricey : priceEstimateY, b2bsetup : B2BSetup});
+                    res.end();
+					client.end();
+				});
+				client.end();
+			});
+		});
+		
+	}
+    else if (req.body.product == "spopB2c") {
+
+        //		Silverpop B2C button was pressed
+
+        var SpopMsg = req.body.spopmsg/1000;
+
+        var q1 = "SELECT quantity_tier,  srp_ref \n" +
+            "FROM public." + tabName + " \n" +
+            "WHERE (part_number = 'D1ILFLL')";
+        console.log("MCB2B query1: " + q1);
+
+        var q2 = "SELECT part_number,  srp_ref \n " +
+            "FROM public." + tabName +  "\n " +
+            "WHERE part_number = 'D1ILBLL' \n " +
+            "OR part_number = 'D1ILILL' \n " +
+            "OR part_number = 'D1ILKLL' \n " +
+            "OR part_number = 'D1ILNLL'";
+
+        console.log("MCB2B query2: " + q2);
+        var client = new pg.Client(postgre_conn_string);
+        client.connect(function(err) {
+            if (err) {
+                res.end("Could not connect to postgre: " + err);
+// 		    	make all references to res return error
+            }
+            console.log("connection opened");
+            client.query(q1, function(err, result) {
+                if (err) {
+                    res.end("Error running query: " + err);
+                }
+                console.log("Output: " + result.rows[0].quantity_tier);
+                var cost = cummulativeTierCalc(result.rows, dbRecords);
+
+                client.query(q2, function(err, result) {
+                    if (err) {
+                        res.end("Error running query: " + err);
+                    }
+                    console.log("returnedValue: " + result);
+                    var B2BSetup = Number(result.rows[3]['srp_ref']);
+                    var StandardAccess = result.rows[0]['srp_ref'];
+                    var mcinsightsP = result.rows[2]['srp_ref'];
+                    var mcusersP = result.rows[1]['srp_ref'];
+
+                    console.log("B2B setup: " + B2BSetup);
+                    console.log("StandardAccess: " + StandardAccess);
+                    console.log("mcinsightsP: " + mcinsightsP);
+                    console.log("mcusersP: " + mcusersP);
+
+//					calculates price from returned value
+                    var priceEstimateM = (cost +
+                    Number(StandardAccess) +
+                    Number(req.body.mcusers)*Number(mcusersP) +
+                    Number(req.body.mcinsights)*Number(mcinsightsP)/100000);
+                    var priceEstimateY = priceEstimateM*12;
+                    console.log("Cost: " + cost);
+                    console.log("PriceM: " + priceEstimateM);
+                    console.log("PriceY: " + priceEstimateY);
+                    console.log("b2bsetup: " + B2BSetup);
+
+                    res.send({pricem : priceEstimateM, 	pricey : priceEstimateY, b2bsetup : B2BSetup});
+                    res.end();
+                    client.end();
+                });
+                client.end();
+            });
+        });
+
+
+
+
+
+
+
+
+
+    }
 
 	
 	});
